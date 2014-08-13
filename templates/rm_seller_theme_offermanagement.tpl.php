4618047562178cebf9a4bddd91b190e21a223c90
allvintage <mfrericks@gmail.com>
2014-08-13 15:09:55 +0200

    Angebotsverwaltung

--- templates/rm_seller_theme_offermanagement.tpl.php
+++ templates/rm_seller_theme_offermanagement.tpl.php
@@ -1,1 +1,70 @@
-hello world
+
+<div class="col-sm-12 col-md-12 main">
+
+    <div class="panel panel-success">
+      <!-- Default panel contents -->
+      <div class="panel-heading"><span class="badge bg-danger pull-right"><a href="#">Komplettes Angebot löschen</a></span><br /><input id="offer_description-title-4523" type="text" class="offer_description form-control" value="" placeholder="Übergordneter Produkttitel, z.B. 'Kaltgepresstes Rapsöl'" /></div>
+      <div class="panel-body">
+        <div class="col-sm-6 col-md-6">
+            <label>Herkunft</label>
+            <div class="col-md-12">
+                <input id="offer_description-field_origin_company-4523" type="text" class="offer_description field_origin_company form-control" value="" placeholder="Firma" />
+            </div>
+            <div class="col-md-4">
+                <input id="offer_description-field_origin_zip-4523" type="text" class="offer_description field_origin_zip form-control" value="" placeholder="PLZ" />
+            </div>
+            <div class="col-md-8">
+                <input id="offer_description-field_origin_city-4523" type="text" class="offer_description field_origin_city form-control" value="" placeholder="Ort" />
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-6">
+            <div class="col-md-4">
+                Siegel 1
+            </div>
+            <div class="col-md-4">
+                Siegel 2
+            </div>
+            <div class="col-md-4">
+                Siegel 3
+            </div>
+            <div class="col-md-4">
+                Siegel 4
+            </div>
+            <div class="col-md-4">
+                Siegel 5
+            </div>
+            <div class="col-md-4">
+                Siegel 6
+            </div>
+            <div class="col-md-4">
+                Siegel 7
+            </div>
+        </div>
+        
+      </div>
+
+      <!-- Table -->
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Angebotsnr.</th>
+            <th>Variante</th>
+            <th>Beschreibung</th>
+            <th>Bilder</th>
+            <th>Produkteinheit</th>
+            <th>Gebinde</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><div class="col-md-12"><input type="text" class="form-control" placeholder="WB-1-772" /></div></td>
+            <td><div class="col-md-12"><input type="text" class="form-control" placeholder="750ml" /></div></td>
+            <td><div class="col-md-12"><input type="text" class="form-control" placeholder="aus der eigenen Mühle, in 750ml Flaschen" /></div></td>
+            <td><div class="col-md-12"><input type="file" class="form-control" /></div></td>
+            <td><div class="col-md-6"><input type="text" class="form-control" placeholder="50" /></div><div class="col-md-6"><input type="text" class="form-control" placeholder="ml" /></div></td>
+            <td><div class="col-md-4"><input type="text" class="form-control" placeholder="6" /></div><div class="col-md-4"><input type="text" class="form-control" placeholder="3,00€" /></div><div class="col-md-4"><input type="text" class="form-control" placeholder="0,00€" /></div></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+</div>
