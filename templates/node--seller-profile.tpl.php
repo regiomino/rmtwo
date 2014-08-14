c35b41b175a1d380af3a86ad5c0b23b52bb5638c
allvintage <mfrericks@gmail.com>
2014-08-14 13:27:22 +0200

    seller template dokumentiert

--- templates/node--seller-profile.tpl.php
+++ templates/node--seller-profile.tpl.php
@@ -1,4 +1,58 @@
 <?php
 
-//Alle spannenden Sachen in $node
-print_r($node->offers);
+/**
+ *  Alle spannenden Sachen in $node
+ */
+
+//Titel
+print $node->title;
+ 
+//Kategorien für den Verkäufer (nicht für die Produkte!)
+foreach($node->field_sellercategories[LANGUAGE_NONE] as $category) {
+    print $category['taxonomy_term']->name;
+}
+ 
+//URLs für Verkäuferbilder
+foreach($node->field_image[LANGUAGE_NONE] as $sellerimage) {
+    print image_style_url('image_style', $sellerimage['uri']);
+}
+
+//Beschreibung
+print $node->body[LANGUAGE_NONE][0]['value'];
+
+//Produkte
+foreach($node->offers as $offer) {
+    //Übergeordneter Titel
+    print $offer->title;
+    //Produktherkunft Firma
+    print $offer->field_origin[LANGUAGE_NONE][0]['organisation_name'];
+    //Produktherkunft PLZ
+    print $offer->field_origin[LANGUAGE_NONE][0]['postal_code'];
+    //Produktherkunft Ort
+    print $offer->field_origin[LANGUAGE_NONE][0]['locality'];
+    
+    foreach($offer->offer_variations as $variation) {
+        //Titel der Produktvariante
+        print $variation->title;
+        //Produkteinheit (Menge)
+        print $variation->field_productunit[LANGUAGE_NONE][0]['first'];
+        //Produkteinheit (Einheit)
+        print $variation->field_productunit[LANGUAGE_NONE][0]['second'];
+        //URLs für Produktbilder
+        foreach($variation->field_image[LANGUAGE_NONE] as $productimage) {
+            print image_style_url('image_style', $productimage['uri']);
+        }
+        //Produktbeschreibung
+        print $variation->body[LANGUAGE_NONE][0]['value'];
+        
+        foreach($variation->trading_units as $tradingunits) {
+            //Gebindemenge
+            print $tradingunits->field_tu_amount[LANGUAGE_NONE][0]['value'];
+            //Gebinde-Nettopreis
+            print number_format($tradingunits->field_tu_price[LANGUAGE_NONE][0]['value'], 2, ",", ".") . ' €';
+            //Gebinde-Pfand
+            print number_format($tradingunits->field_tu_deposit[LANGUAGE_NONE][0]['value'], 2, ",", ".") . ' €';
+        }
+    }
+}
+?>
