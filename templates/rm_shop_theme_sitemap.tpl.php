99003bafec52c06a1b326ea0818ec9c058c5e571
allvintage <mfrericks@gmail.com>
2014-08-15 13:08:27 +0200

    Sitemap

---
+++ templates/rm_shop_theme_sitemap.tpl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Martin
+ * Date: 06.08.14
+ * Time: 16:16
+ */
+?>
+<div class="col-md-12">
+    <?php if(!empty($vars['sitemap'])): ?>
+    
+    <?php endif; ?>
+</div>
