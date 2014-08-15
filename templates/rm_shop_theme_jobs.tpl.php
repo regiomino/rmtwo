be31faca87af0d83ddcfdae7d26a099cf4b7225d
allvintage <mfrericks@gmail.com>
2014-08-15 12:09:05 +0200

    Jobs und Presse

--- templates/rm_shop_theme_jobs.tpl.php
+++ templates/rm_shop_theme_jobs.tpl.php
@@ -6,4 +6,26 @@
  * Time: 16:16
  */
 ?>
-<div class="col-md-12"><?php print $vars['jobs']; ?></div>
+<div class="col-md-12">
+    <?php if(!empty($vars['jobs'])): ?>
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>Position</th>
+                <th>Standort</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($vars['jobs'] as $nid => $job): ?>
+                <tr>
+                    <td><?php print l($job->title, 'node/' . $nid); ?></td>
+                    <td><?php print drupal_render(addressfield_generate($job->field_jobaddress[LANGUAGE_NONE][0], array('address' => 'address'), array('mode' => 'render')));
+                    $output = drupal_render($address_render_array); ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+    <?php else: ?>
+    <p>Derzeit sind bei uns keine Stellen offen</p>
+    <?php endif; ?>
+</div>
