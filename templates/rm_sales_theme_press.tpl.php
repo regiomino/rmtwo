0d258597768f85a9e383fbd21c7619e6dc762e2c
allvintage <mfrericks@gmail.com>
2014-08-15 11:04:08 +0200

    Pressebereich

--- templates/rm_sales_theme_press.tpl.php
+++ templates/rm_sales_theme_press.tpl.php
@@ -1,4 +1,26 @@
 <div class="col-md-12">
+<div class="media">
+  <a class="pull-left" href="<?php print file_create_url($vars['logo']->field_file[LANGUAGE_NONE][0]['uri']); ?>">
+    <?php
+        $thumburi = '';
+        if(empty($vars['logo']->field_media_thumb[LANGUAGE_NONE][0]['uri'])) {
+            $field_info = field_info_field('field_media_thumb');
+            $fid = $field_info['settings']['default_image'];
+            $image = file_load($fid);
+            $thumburi = $image->uri;
+        }
+        else {
+            $thumburi = $vars['logo']->field_media_thumb[LANGUAGE_NONE][0]['uri'];
+        }
+    ?>
+    <img class="media-object" src="<?php print image_style_url('icon', $thumburi); ?>" alt="Download">
+  </a>
+  <div class="media-body">
+    <h4 class="media-heading"><?php print $vars['logo']->title; ?></h4>
+    <?php print $vars['logo']->body[LANGUAGE_NONE][0]['value']; ?>
+  </div>
+</div>
+<br />
 <table class="table table-bordered">
     <thead>
         <tr>
