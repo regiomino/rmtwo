750ac91d44bbc4824a72220bb1ce55fcb6e2206e
allvintage <mfrericks@gmail.com>
2014-08-16 08:36:14 +0200

    Sitemap mit Inhalt gefuellt

--- templates/rm_shop_theme_sitemap.tpl.php
+++ templates/rm_shop_theme_sitemap.tpl.php
@@ -8,6 +8,13 @@
 ?>
 <div class="col-md-12">
     <?php if(!empty($vars['sitemap'])): ?>
-    
+        <?php foreach($vars['sitemap'] as $type => $nodeobjects): ?>
+            <h4><?php print node_type_get_name($type); ?></h4>
+            <ul class="list-unstyled">
+            <?php foreach($nodeobjects as $nodeobject): ?>
+                <li><?php print l($nodeobject->title, 'node/' . $nodeobject->nid); ?></li>
+            <?php endforeach; ?>
+            </ul>
+        <?php endforeach; ?>
     <?php endif; ?>
 </div>
