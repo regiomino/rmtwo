ac3cd6c707790b936bd1e7711aadbba51a7f8a14
root <root@ubuntu.(none)>
2014-08-18 13:01:46 +0200

    Hinzufuegen der rm_cart.js im Verkauefershop

--- template.php
+++ template.php
@@ -366,6 +366,7 @@ function rmtwo_preprocess_page(&$variables) {
         if(array_key_exists('node', $variables)) {
             if($variables['node']->type == 'seller_profile') {
                 $variables['node']->offers = rm_shop_get_structured_seller_offers($variables['node']->uid);
+                drupal_add_js(drupal_get_path('module', 'rm_cart') . '/js/rm_cart.js');
             }
         }
     }
