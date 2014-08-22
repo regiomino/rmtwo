4d8a6aae74ee0cd61650326535fa55f011e85f7e
allvintage <mfrericks@gmail.com>
2014-08-22 09:54:22 +0200

    Dynamisierung des Warenkorbs und verkaeuferprofils

--- templates/rm_cart_theme_cart_block.tpl.php
+++ templates/rm_cart_theme_cart_block.tpl.php
@@ -1,10 +1,33 @@
- 
+<?php
+    $tupackaging = list_allowed_values(field_info_field('field_tu_packaging'));
+    $packaging_field = field_info_field('field_tu_packaging');
+    $packaging_instance = field_info_instance('node', 'field_tu_packaging', 'trading_unit');
+?>
 
 <div id="cart" class="cart-wrapper">
    <div class="cart-header">
         <h3><span class="glyphicon glyphicon-shopping-cart"> </span> Warenkorb</h3>
    </div>
     <div class="cart-content">
+
+        <?php if(!empty($vars['cart'])): foreach($vars['cart'] as $cart_item): ?>
+        <?php
+            $tupackaging = list_allowed_values(field_info_field('field_tu_packaging'));
+            $packaging_field = field_info_field('field_tu_packaging');
+            $packaging_instance = field_info_instance('node', 'field_tu_packaging', 'trading_unit');
+            $variation = node_load($cart_item->field_offer_variation_reference[LANGUAGE_NONE][0]['target_id']);
+            $tradingunit = node_load($cart_item->field_trading_unit_reference[LANGUAGE_NONE][0]['target_id']);
+            $options = rm_cart_get_quantity_options($cart_item->field_offer_variation_reference[LANGUAGE_NONE][0]['target_id']);
+            $item_total = number_format(rm_cart_get_item_total($cart_item->nid), 2, ",", ".");
+            $unit_description = $tradingunit->field_tu_amount[LANGUAGE_NONE][0]['value'] . ' &times; ' . $variation->field_productunit[LANGUAGE_NONE][0]['first'] . t($variation->field_productunit[LANGUAGE_NONE][0]['second']);
+            if(isset($tradingunit->field_tu_packaging[LANGUAGE_NONE][0]['value']) && !empty($tupackaging[$tradingunit->field_tu_packaging[LANGUAGE_NONE][0]['value']])) {
+                $packaging = $tupackaging[$tradingunit->field_tu_packaging[LANGUAGE_NONE][0]['value']];
+            }
+            else {
+                $default = field_get_default_value('node', $tradingunit, $packaging_field, $packaging_instance, $tradingunit->language);
+                $packaging = $tupackaging[$default[0]['value']];
+            }
+        ?>
         <div class="cart-item">
             <div class="row">
                 <div class="col-xs-5">
@@ -19,87 +42,16 @@
                     </div>
                 </div>
                 <div class="col-xs-7">
-                    <p class="product-title"> <small> Fleischwurst für Currywurst extra lecker, schmeckt nicht wirklich lecker.
+                    <p class="product-title"> <small> <?php print $variation->title; ?><br><small><?php print $packaging; ?> (<?php print $unit_description; ?>)</small>
                     </small>
                     </p>
                     <p class="price text-right">
-                        <strong><small>20,34 € </small> </strong>
+                        <strong><small><?php print $item_total; ?>€ </small> </strong>
                     </p>
                 </div>
             </div>
         </div>
-        
-        <div class="cart-item">
-            <div class="row">
-                <div class="col-xs-5">
-                    <div class="input-group">
-                        <span class="input-group-btn">
-                            <button class=" btn  btn-default"><span class="glyphicon glyphicon-plus"></span></button>
-                        </span>
-                        <input type="number" class="form-control">
-                        <span class="input-group-btn">
-                            <button class=" btn  btn-default"><span class="glyphicon glyphicon-minus"></span></button>
-                        </span>
-                    </div>
-                </div>
-                <div class="col-xs-7">
-                    <p class="product-title"> <small> Truthahn, gefüllt mir Würstchen
-                    </small>
-                    </p>
-                    <p class="price text-right">
-                        <strong><small>2000,34 € </small> </strong>
-                    </p>
-                </div>
-            </div>
-        </div>
-        
-        <div class="cart-item">
-            <div class="row">
-                <div class="col-xs-5">
-                    <div class="input-group">
-                        <span class="input-group-btn">
-                            <button class=" btn  btn-default"><span class="glyphicon glyphicon-plus"></span></button>
-                        </span>
-                        <input type="number" class="form-control">
-                        <span class="input-group-btn">
-                            <button class=" btn  btn-default"><span class="glyphicon glyphicon-minus"></span></button>
-                        </span>
-                    </div>
-                </div>
-                <div class="col-xs-7">
-                    <p class="product-title"> <small> Truthahn, gefüllt mir Würstchen
-                    </small>
-                    </p>
-                    <p class="price text-right">
-                        <strong><small>2000,34 € </small> </strong>
-                    </p>
-                </div>
-            </div>
-        </div>
-        
-        <div class="cart-item">
-            <div class="row">
-                <div class="col-xs-5">
-                    <div class="input-group">
-                        <span class="input-group-btn">
-                            <button class=" btn  btn-default"><span class="glyphicon glyphicon-plus"></span></button>
-                        </span>
-                        <input type="number" class="form-control">
-                        <span class="input-group-btn">
-                            <button class=" btn  btn-default"><span class="glyphicon glyphicon-minus"></span></button>
-                        </span>
-                    </div>
-                </div>
-                <div class="col-xs-7">
-                    <p class="product-title"> <small> Truthahn, gefüllt mir Würstchen
-                    </small>
-                    </p>
-                    <p class="price text-right">
-                        <strong><small>2000,34 € </small> </strong>
-                    </p>
-                </div>
-            </div>
-        </div>
+        <?php endforeach; endif; ?>
         
         <!--
           <ul class="list-unstyled"> 
@@ -129,8 +81,9 @@
     </ul>-->
     </div>
      <div class="cart-summary">
-        <p class="pre-sum text-muted">Zwischensumme  <span class="pull-right"><?php print number_format(rm_cart_get_cart_total(), 2, ",", "."); ?>€ </span></p>
-       <p class="sum"><strong>Gesamtbetrag</strong> <span class="pull-right"><strong> <?php print number_format(rm_cart_get_cart_total(), 2, ",", "."); ?>€</strong> </p>
+        <p class="pre-sum text-muted">Summe  <span class="pull-right"><?php print number_format(rm_cart_get_cart_total(), 2, ",", "."); ?>€ </span></p>
+        <p class="pre-sum text-muted">zzgl. MwSt.  <span class="pull-right"><?php print number_format(rm_cart_get_cart_vat(), 2, ",", "."); ?>€ </span></p>
+       <p class="sum"><strong>Gesamtbetrag</strong> <span class="pull-right"><strong> <?php print number_format(rm_cart_get_cart_total() + rm_cart_get_cart_vat(), 2, ",", "."); ?>€</strong> </p>
         
         <?php print l(t('Purchase now'), 'checkout', array('external' => TRUE, 'attributes' => array('class' => array('btn', 'btn-primary', 'btn-lg', 'center-block')))); ?>
      </div>
