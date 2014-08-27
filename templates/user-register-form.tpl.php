5d47a46aa26b0cd101b28900ffcb2fffaa94c2e9
allvintage <mfrericks@gmail.com>
2014-08-27 07:27:22 +0200

    Registrierungsformular durch Vorschlagen-Formular ersetzt

--- templates/user-register-form.tpl.php
+++ templates/user-register-form.tpl.php
@@ -7,6 +7,6 @@
  */
 ?>
 <div class="col-md-8">
-    <h3><?php print t('... or register a new account'); ?></h3>
-    <?php print drupal_render_children($form); ?>
+    <h3><?php print t('... or apply for a new account'); ?></h3>
+    <?php print drupal_render_children(drupal_get_form('rm_sales_suggest_form')); ?>
 </div>
