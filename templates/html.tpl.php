ec201cca4b23dc10f354687909f1e90e9075aa7a
Arcade1080 <root@dev2.pbiaas.com>
2014-08-28 17:18:33 +0200

    sfsf

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -80,7 +80,15 @@
                                 <?php } ?>
     </script>
     
-    
+    <script type="text/javascript">
+var fby = fby || [];
+fby.push(['showTab', {id: '7820', position: 'left', color: '#95BC0D'}]);
+(function () {
+    var f = document.createElement('script'); f.type = 'text/javascript'; f.async = true;
+    f.src = '//cdn.feedbackify.com/f.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(f, s);
+})();
+</script>
 
 </body>
 </html>
