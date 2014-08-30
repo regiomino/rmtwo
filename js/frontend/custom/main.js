95ee432e51784fa1f4e98fc6f4d6652e1c9c997e
Arcade1080 <root@dev2.pbiaas.com>
2014-08-30 13:57:41 +0200

    Fehlende Files nach-committed

---
+++ js/frontend/custom/main.js
@@ -0,0 +1,41 @@
+ 
+jQuery(document).ready(function ($) {
+    
+    if ( !Modernizr.input.placeholder) {
+        $('body').addClass('no-placeholder');
+    }
+
+    $('#read-more').on('click', function(e){
+        e.preventDefault();
+        $('#short-desc').hide();
+        $('#long-desc').show();
+    });
+    
+     $('#read-less').on('click', function(e){
+        e.preventDefault();
+        $('#long-desc').hide();
+        $('#short-desc').show();
+    });
+
+    
+    
+    $('#partner-logos a').tooltip();
+    
+    //messages are placed inside a bootstrap modal #messageModal. this triggers it, when it's there.
+    $('#messageModal').modal('show');
+
+    //makes sure external links are opened in new window
+    $('a').each(function() {
+        var a = new RegExp('/' + window.location.host + '/');
+        if(!a.test(this.href)) {
+            $(this).click(function(event) {
+                event.preventDefault();
+                event.stopPropagation();
+                window.open(this.href, '_blank');
+            });
+        }
+    });
+  
+    
+    
+});
