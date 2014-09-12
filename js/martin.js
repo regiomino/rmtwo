118ed37a85ae7115e3c4b44e65b4e8715b4ed61d
allvintage <mfrericks@gmail.com>
2014-09-12 11:06:21 +0200

    Suggest Modal per URL ansprechbar

--- js/martin.js
+++ js/martin.js
@@ -2,4 +2,18 @@ jQuery(document).ready(function ($) {
     if($('#suggestModal #edit-mollom-captcha').hasClass('error')) {
         $('#suggestModal').modal('show');
     }
+    if($('#suggestModal #edit-mollom-captcha').hasClass('error')) {
+        $('#suggestModal').modal('show');
+    }
+    
+    function getParameterByName(name) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+            results = regex.exec(location.search);
+        return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+    var suggest = getParameterByName('suggest');
+    if(suggest == 1) {
+        $('#suggestModal').modal('show');
+    }
 });
