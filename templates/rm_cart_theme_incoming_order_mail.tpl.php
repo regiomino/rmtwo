fa171bd56f9607396a583fa5b6a63fd7c2f7bbb4
allvintage <mfrericks@gmail.com>
2014-10-08 13:34:07 +0200

    Email Versand ueber templates vorbereitet

--- templates/rm_cart_theme_incoming_order_mail.tpl.php
+++ templates/rm_cart_theme_incoming_order_mail.tpl.php
@@ -1,3 +1,19 @@
+<?php
+    /*
+    $vars['order_number'];
+    $vars['user_gender'];
+    $vars['user_first_name'];
+    $vars['user_last_name'];
+    $vars['order_items'];
+    $vars['billing_address'];
+    $vars['shipping_address'];
+    $vars['delivery_type'];
+    $vars['payment_type'];
+    $vars['pickup_agreement'];
+    $vars['delivery_range_from'];
+    $vars['delivery_range_to'];
+    */
+?>
 <h2>Vielen Dank!</h2>
 <p>Ihre Bestellung ist bei uns eingegangen und wir haben sie an Ihren Lieferanten weitergeleitet.</p>
 <p>Ihr Regiomino-Team</p>
