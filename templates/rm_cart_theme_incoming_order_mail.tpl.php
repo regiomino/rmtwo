de46437f09336e245224370e5b082f9930f20840
allvintage <mfrericks@gmail.com>
2014-10-08 11:12:15 +0200

    Email Versand ueber templates vorbereitet

--- templates/rm_cart_theme_incoming_order_mail.tpl.php
+++ templates/rm_cart_theme_incoming_order_mail.tpl.php
@@ -1,1 +1,3 @@
-Dies ist ein Test
+<h2>Vielen Dank!</h2>
+<p>Ihre Bestellung ist bei uns eingegangen und wir haben sie an Ihren Lieferanten weitergeleitet.</p>
+<p>Ihr Regiomino-Team</p>
---
+++ templates/rm_cart_theme_incoming_payment_mail.tpl.php
@@ -0,0 +1,3 @@
+<h2>Wir haben Ihre <?php print $vars['provider']; ?>-Zahlung erhalten!</h2>
+<p>Wir haben Ihre <?php print $vars['provider']; ?>-Zahlung über <?php print number_format($vars['totalamount'], 2, ",", "."); ?>€ für die Bestellung <?php print $vars['order_number']; ?> erhalten.</p>
+<p>Ihr Regiomino-Team</p>
