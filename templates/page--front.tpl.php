77e90322eb30f6fbcae358dba2a7fecff48a6857
allvintage <mfrericks@gmail.com>
2014-10-15 10:03:08 +0200

    Footer Hinweis auf Gewerbe-angebot

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -339,6 +339,9 @@ Kooperativen wird vereinfacht, weil die Beschaffungskosten geringer werden.</li>
             </div>
         </div>
         <div class="row">
+                <div class="col-md-12"><p class="text-center small">Die auf www.regiomino.de gezeigten Angebote richten sich ausschließlich an Unternehmen und Gewerbetreibende. Alle Preise sind Netto-Preise zzgl. der beim Angebot genannten MwSt.</p></div>
+            </div>
+        <div class="row">
             <div class="col-md-12"><p class="text-center">&copy; Regiomino 2014</p></div>
         </div>
     </div>   
