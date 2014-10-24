7a8723a75ef1795f8edd6b55db554166c3b8a2fc
root <root@ubuntu.(none)>
2014-10-24 14:27:32 +0200

    sf

--- js/frontend/module_only/mapsndshops/ms.js
+++ js/frontend/module_only/mapsndshops/ms.js
@@ -1,148 +1,11 @@
 jQuery(document).ready(function ($) {
 
- /*var debounce = function(func, wait, immediate) {
-     
-        var timeout;
-        return function() {
-                var context = this, args = arguments;
-                clearTimeout(timeout);
-                timeout = setTimeout(function() {
-                        timeout = null;
-                        if (!immediate) func.apply(context, args);
-                }, wait);
-                if (immediate && !timeout) func.apply(context, args);
-        };
-    };
-    
-    var viewport = function () {
-        var e = window, a = 'inner';
-        if (!('innerWidth' in window )) {
-            a = 'client';
-            e = document.documentElement || document.body;
-        }
-        return { width : e[ a+'Width' ] , height : e[ a+'Height' ] };
-    };
-    
-    
-    var adjustWinH = debounce(function(){
-        var w = viewport(),
-            vpW = w.width;
-        
-        
-        var sH = (vpW >= 768 )?(w.height - 65): 300;
-        
-            $('.sidebar').css({
-                'height' : sH + 'px'
-            });
-            var center = map.getCenter();
-            google.maps.event.trigger(map, "resize");
-            map.setCenter(center);
-        
-        },200);
-    
-    
-    $(window).on('resize.wind', adjustWinH).resize();
-    
-    $('.label-details').click(function(e) { e.preventDefault(); }).popover({
-      trigger: 'click hover',
-      html: true
-    })
+ $.ajaxSetup({
+             'beforeSend':function () {
+                          console.log(this.url);}
+           });
 
-    $('#filterShops').keyup(function () {
-        var rex = new RegExp($(this).val(), 'i');
-        $('.col-seller-item').hide();
-        $('.col-seller-item').filter(function () {
-            return rex.test($(this).data('title'));
-        }).show();
-        
-    });
-    
-    var pathToTheme = Drupal.settings.basePath + "sites/all/themes/" + Drupal.settings.ajaxPageState.theme;
-
-    var customIcons = {
-        inactive_profile: {
-            icon: pathToTheme + '/images/markers/inactive_profile.png',
-            zindex: 1
-        },
-        prospect_profile: {
-            icon: pathToTheme + '/images/markers/inactive_profile.png',
-            zindex: 2
-        },
-        customer_profile: {
-            icon: pathToTheme + '/images/markers/customer_profile.png',
-            zindex: 3
-        },
-        seller_profile: {
-            icon: pathToTheme + '/images/markers/seller_profile.png',
-            zindex: 4
-        },
-    };
-
-    
-
-    var latlng = [];
-
-    var infoWindow = new google.maps.InfoWindow;
-
-    downloadUrl(Drupal.settings.basePath + 'rm-shop-participantxml', function(data) {
-        var xml = data.responseXML;
-        var markers = xml.documentElement.getElementsByTagName("marker");
-        for (var i = 0; i < markers.length; i++) {
-            var name = markers[i].getAttribute("name");
-            var address = markers[i].getAttribute("address");
-            var type = markers[i].getAttribute("type");
-            
-            var point = new google.maps.LatLng(
-                parseFloat(markers[i].getAttribute("lat")),
-                parseFloat(markers[i].getAttribute("lng")));
-            if(type != 'inactive_profile') latlng.push(point);
-            var html = "<b>" + name + "</b> <br/>" + address;
-            var icon = customIcons[type] || {};
-            var marker = new google.maps.Marker({
-                map: map,
-                position: point,
-                icon: icon.icon,
-                zIndex: icon.zindex
-            });
-            bindInfoWindow(marker, map, infoWindow, html);
-        }
-        google.maps.event.trigger(map, "resize");
-        var latlngbounds = new google.maps.LatLngBounds();
-        for (var i = 0; i < latlng.length; i++) {
-            latlngbounds.extend(latlng[i]);
-        }
-        map.setCenter(latlngbounds.getCenter());
-        map.fitBounds(latlngbounds);
-    });
-
-    function downloadUrl(url,callback) {
-        var request = window.ActiveXObject ?
-            new ActiveXObject('Microsoft.XMLHTTP') :
-            new XMLHttpRequest;
-
-        request.onreadystatechange = function() {
-            if (request.readyState == 4) {
-                request.onreadystatechange = doNothing;
-                callback(request, request.status);
-            }
-        };
-
-        request.open('GET', url, true);
-        request.send(null);
-    }
-
-    function bindInfoWindow(marker, map, infoWindow, html) {
-        google.maps.event.addListener(marker, 'click', function() {
-            infoWindow.setContent(html);
-            infoWindow.open(map, marker);
-        });
-    }
-    
-    function doNothing(){
-        
-    }*/
-
-
+ 
 
 var RMS = RMS || {};
 window.RMS = RMS;
@@ -152,6 +15,8 @@ RMS.init = function(){
     RMS.map.init();
     RMS.filter.init();
 };
+
+ 
 
 //////////////////////////////////
 // RM Ajax
@@ -166,17 +31,18 @@ RMS.ajax.PATH_GET_LOCATIONS = '/rm-shop-filter';
 
 RMS.ajax.Query = function() {
     
-    var c = Drupal.settings.rm_shop.map_center.split(',');
+   // var c = Drupal.settings.rm_shop.map_center.split(',');
     
     this.qvalues = {
         map_bounds_ne_lat : Drupal.settings.rm_shop.map_bounds_ne_lat,
         map_bounds_ne_lng : Drupal.settings.rm_shop.map_bounds_ne_lng,
         map_bounds_sw_lat : Drupal.settings.rm_shop.map_bounds_sw_lat,  
         map_bounds_sw_lng : Drupal.settings.rm_shop.map_bounds_sw_lng,
-        map_center : {
+        /*map_center : {
             lat : parseFloat(c[0]),
             lng : parseFloat(c[1])
-        },
+        },*/
+        map_center : Drupal.settings.rm_shop.map_center,
         seller_name : Drupal.settings.rm_shop.seller_name,
         delivery_option : Drupal.settings.rm_shop.delivery_options,
         payment_type : Drupal.settings.rm_shop.payment_type,
@@ -211,22 +77,28 @@ RMS.ajax.addLoader = function(){
 RMS.ajax.removeLoader = function(){
     RMS.ajax.$sellerArea.removeClass(RMS.ajax.LOADER_CLASSNAME);
 }
- 
+  
 RMS.ajax.updateResults = function(){
     var _self = this;
     _self.addLoader();
+    var url = window.location.href;
+    var prefix;
+    var newUrl;
+     
+    prefix = (url.indexOf(_self.PATH_GET_LOCATIONS) != -1) ? url.substring(0,url.indexOf(_self.PATH_GET_LOCATIONS)) : prefix = url;
     
+    newUrl = prefix + _self.PATH_GET_LOCATIONS + '?' + $.param(_self.sq.getQuery());
+    history.pushState({}, '',newUrl);
     
     $.ajax({
-        url: _self.PATH_GET_LOCATIONS+/'tEst'/,
+        url: _self.PATH_GET_LOCATIONS,
         type: "GET",
         data :  _self.sq.getQuery(),
-        dataType : 'json'
+        dataType : 'json',
   
     }).success(function(data) {
        if (data.html != undefined) {
             RMS.$sellerItemsArea.html(data.html);
-           
             RMS.map.updateMarker(data.marker);
              _self.removeLoader();
        
@@ -243,8 +115,18 @@ RMS.ajax.updateResults = function(){
 
 RMS.map = {};
 RMS.map.mapContainer = "map";
+Drupal.settings.rm_shop.map_center
+RMS.map.getCenter = function(){
+    var _self = this;
+    var c = Drupal.settings.rm_shop.map_center.split(',');
+    var map_center = {
+        lat : parseFloat(c[0]),
+        lng : parseFloat(c[1])
+    };
+    return map_center;
+};
 RMS.map.mapOptions = {
-    center: RMS.ajax.sq.qvalues.map_center,  
+    center: RMS.map.getCenter(),  
     zoom: 11,
     mapTypeId: 'roadmap'
 };
