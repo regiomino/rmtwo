04da4f63cd779217de84ab59135befccaa645b72
root <root@ubuntu.(none)>
2014-11-10 12:27:53 +0100

    Neues aboutus template hinzugefgt

---
+++ templates/rm_shop_theme_aboutus.tpl.php
@@ -0,0 +1,15 @@
+<div class="row">
+    <div class="col-md-12">
+       <p> Regiomino ist ein Internet-Marktplatz, auf dem Sie regionale Lebensmittel einkaufen und verkaufen können. Einfach und zu fairen Preisen!
+
+Immer mehr Menschen möchten sich gesünder ernähren und die regionale Angebotsvielfalt nutzen. Regionale Produkte sind überall vorhanden, nur fällt es schwer sie zu beschaffen.
+
+Regiomino löst dieses Problem mit einem einfachen Internet-Marktplatz für regionale Produkte:
+
+Produzenten bieten ihre Produkte direkt auf der Regiomino Plattform an. Damit reduziert Regiomino den Aufwand bei der Direktvermarktung und macht diese vor allem für kleinere Produzenten erst möglich.
+
+Kunden können Produkte aus der Region ihrer Wahl einkaufen und werden sogar beliefert, d.h. sparen Zeit. Die Herkunft der Produkte wird sichtbar und das schafft Sicherheit.
+
+Regiomino kürzt den Prozess der Beschaffung und Vermarktung ab und das Geld bleibt in unserem, regionalen Wirtschaftskreislauf.</p> 
+    </div>
+</div>
