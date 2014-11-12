e9e786da546572151868c2cf1761192b0468143a
Julian Scheele <juscheele@gmail.com>
2014-11-12 15:53:50 +0100

    Typeahead mit Testdaten, ohne Bloodhound, ohne Styles

--- js/frontend/module_only/mapsndshops/ms.js
+++ js/frontend/module_only/mapsndshops/ms.js
@@ -347,7 +347,8 @@ RMS.filter.filterArea = $(RMS.filter.filterAreaID);
 RMS.filter.init = function() {
     var _self = this;
     
-    _self.search.init();
+   // _self.search.init();
+    _self.ta.init();
     _self.category.init();
    // _self.distance.init();
     _self.addListeners();
@@ -522,6 +523,69 @@ RMS.filter.search.clearInput = function(){
     var _self = this;
     _self.$input.val('');
     _self.$input.trigger('keyup');
+}
+
+/////////////////////////////////////     
+//Typeahead Filter
+/////////////////////////////////////
+
+RMS.filter.ta = {};
+RMS.filter.ta.substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substrRegex;
+ 
+    // an array that will be populated with substring matches
+    matches = [];
+ 
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+ 
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        // the typeahead jQuery plugin expects suggestions to a
+        // JavaScript object, refer to typeahead docs for more info
+        matches.push({ value: str });
+      }
+    });
+ 
+    cb(matches);
+  };
+};
+RMS.filter.ta.data2 = ['Apple', 'Oranges', 'Bananas', 'Cranberry', 'Sugar', 'Onion'];
+
+RMS.filter.ta.data1 = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+];
+RMS.filter.ta.init = function(){
+    var _self = this;
+    $('#filterShops').typeahead({
+            highlight: true
+        },
+        {
+            name: 'nba-teams',
+            displayKey: 'team',
+            source: _self.substringMatcher(_self.data2),
+            templates: {
+                header: '<h4 class="league-name">NBA Teams</h4>'
+            }
+        },
+        {
+            name: 'nhl-teams',
+            displayKey: 'team',
+            source: _self.substringMatcher(_self.data1),
+            templates: {
+                header: '<h3 class="league-name">NHL Teams</h3>'
+        }
+    });
 }
 
 /////////////////////////////////////     
