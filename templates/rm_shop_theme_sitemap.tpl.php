bbd793ba8c4e34433372efa0815454e05e4840ce
allvintage <mfrericks@gmail.com>
2014-11-16 19:18:25 +0100

    Performance Optimierungen

--- templates/rm_shop_theme_sitemap.tpl.php
+++ templates/rm_shop_theme_sitemap.tpl.php
@@ -9,11 +9,11 @@
 <div class="row">
 <div class="col-md-12">
     <?php if(!empty($vars['sitemap'])): ?>
-        <?php foreach($vars['sitemap'] as $type => $nodeobjects): ?>
+        <?php foreach($vars['sitemap'] as $type => $values): ?>
             <h4><?php print node_type_get_name($type); ?></h4>
             <ul class="list-unstyled">
-            <?php foreach($nodeobjects as $nodeobject): ?>
-                <li><?php print l($nodeobject->title, 'node/' . $nodeobject->nid); ?></li>
+            <?php foreach($values as $value): ?>
+                <li><?php print l($value['title'], 'node/' . $value['nid']); ?></li>
             <?php endforeach; ?>
             </ul>
         <?php endforeach; ?>
